4d770de0cdfb31566c405e939ab92cfedc258667
Shraddha Maku <145137547+MakuShraddha@users.noreply.github.com>
2025-03-29 21:16:44 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from 'react';
-import Navbar from './component/Navbar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import "./App.css";
-import{BrowserRouter as Router,Route,Routes} from 'react-router-dom' ;
-function App () {
+import React from 'react'
+import Navbar from './Component/Navbar'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+
+const App = () => {
   return (
     <Router>
-      <Navbar />
-      <Home />
-      <Routes>
-        <Route path="/home"element={<Home />} />
-        <Route path="/login"element={<Login />} />
-        <Route path="/register"element={<Register />} />
-        
-      </Routes>
-    </Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </Router>
   )
 }
-
-export default App
+export default App
